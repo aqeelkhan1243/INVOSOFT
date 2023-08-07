157c70fd8646711e7e54e53b7ccb83229a6bda0d
Aqeel <134676848+aqeelkhan1243@users.noreply.github.com>
2023-08-07 11:05:33 +0500

    Add files via upload

---
+++ Invosoft/contact.php
@@ -0,0 +1,416 @@
+<?php
+	$serverName = "";
+	$username = "";
+	$password = "";
+	$dataBase = "";
+
+?>
+
+<!DOCTYPE html>
+<html  class="no-js" lang="en">
+
+<!-- Mirrored from preetheme.com/html/cwt/cwt/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 05 Jul 2023 07:00:38 GMT -->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>CWT - Personal Portfolio Website HTML5 Template</title>
+	<link rel="icon" href="assets/img/Invosoft.png	" type="image/gif" sizes="16x16">
+	<link rel="icon" href="assets/img/Invosoft.png	" type="image/gif" sizes="18x18">
+	<link rel="icon" href="assets/img/Invosoft.png	" type="image/gif" sizes="20x20">
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/fontawesome.all.min.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/typed.css">
+	<link rel="stylesheet" href="assets/css/lightcase.css">
+	<link rel="stylesheet" href="assets/css/normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+<body>
+	<!-- Preloader -->
+	<div id="preloader">
+      	<div class="preloader">
+          	<span></span>
+          	<span></span>
+      	</div>
+	</div>
+	<!-- Start Header Area -->
+	<!-- Start Header Area -->
+	<header class="header transparent_header">
+		<div class="container">
+			<div class="row">
+				<!-- Logo -->
+				<div class="col-lg-2 ">
+					<div class="logo">
+						<a href="index.html">
+							<img src="./assets/img/logo-white.png" alt="logo" class="img-fluid main-logo">
+						</a>
+					</div>
+					<div class="mobile-bar">
+						<div class="canvas_open">
+					        <a href="javascript:void(0)">
+					        	<span></span>
+					        	<span></span>
+					        	<span></span>
+					        </a>
+					    </div>
+					</div>
+				</div>
+				<!-- Menu -->
+				<div class="col-lg-10">
+					<!-- Top Right -->
+					<div class="top-right mt-1">
+						<div class="top-right-btn">
+							<i class="fas fa-bars"></i>
+						</div>
+					</div>
+					<!-- Menu -->
+					<div class="menu align-self-center mt-3">
+						<nav>
+							<ul>
+								<li>
+									<a href="index.html" class="mt-2">Home</a>
+								</li>
+								<li><a href="about.html" class="mt-2">About Us</a></li>
+								<li>
+									<a href="services.html" class="mt-2">Services</a>
+									<ul>
+										<li><a href="services-details.html">Services Details</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="portfolio.html" class="mt-2">Portfolio</a>
+									<ul>
+										<li><a href="portfolio-details.html">Portfolio Details</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html" class="mt-2">Contact Us</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- End Header Area -->
+	<!-- Start Mobile Menu Area -->
+	<div class="mobile-menu-area">
+		
+		<!--offcanvas menu area start-->
+	    <div class="off_canvars_overlay">
+	                
+	    </div>
+	    <div class="offcanvas_menu">
+	        <div class="offcanvas_menu_wrapper">
+	            <div class="canvas_close">
+	                <a href="javascript:void(0)"><i class="fas fa-times"></i></a>  
+	            </div>
+	            <div class="mobile-logo">
+	            	<a href="index.html">
+	            		<img src="assets/img/Invosoft.png" alt="logo" class="img-fluid w-25">
+	            	</a>
+	            </div>
+	            <div id="menu" class="text-left ">
+	                <ul class="offcanvas_main_menu">
+	                    <li class="menu-item-has-children active">
+	                        <a href="index.html">Home</a>
+	                    <li>
+	                        <a href="about.html"> About Us</a> 
+	                    </li>
+	                    <li class="menu-item-has-children">
+	                        <a href="services.html">Services</a>
+	                        <ul class="sub-menu">
+								<li><a href="services-details.html">Services Details</a></li>
+	                        </ul>
+	                    </li>
+	                    <li class="menu-item-has-children">
+	                        <a href="portfolio.html">Portfolio</a>
+	                    </li>
+	                    <li>
+	                        <a href="contact.html"> Contact Us</a> 
+	                    </li>
+	                </ul>
+	            </div>
+	        </div>
+	    </div>
+    </div>
+    <!--offcanvas menu area end-->
+	<!-- End Header Area -->
+	<div class="off_canvars_overlay"></div>
+	<!-- Sidebar Cnavas Content -->
+	<div class="sidebar_main_content">
+		<div class="sidebar_main_content_full">
+			<div class="sidebar_he_a">
+				<div class="sidebar-close">
+					<i class="fas fa-times"></i>
+				</div>
+				<h2>About Us</h2>
+			</div>
+			<div class="content">
+				<div class="item">
+					<div class="img">
+						<img src="assets/img/me.png" alt="img">
+					</div>
+				</div>
+				<div class="item">
+					<h4>Name :</h4>
+					<p>Ahsan Zaheer</p>
+				</div>
+				<div class="item">
+					<h4>Email :</h4>
+					<p>zaheerahsan775@gmail.com</p>
+				</div>
+				<div class="item">
+					<h4>Phone :</h4>
+					<p>(92) 3494 1084 497</p>
+				</div>
+				<div class="item">
+					<h4>Address :</h4>
+					<p>Sabzazar, Lahore, Pakistan.</p>
+				</div>
+				<div class="item">
+					<h4>About Me :</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown </p>
+				</div>
+				<div class="item">
+					<div class="side_social">
+						<span><a href="#"><i class="fab fa-facebook-f"></i></a></span>
+						<span><a href="#"><i class="fab fa-twitter"></i></a></span>
+						<span><a href="#"><i class="fab fa-instagram"></i></a></span>
+						<span><a href="#"><i class="fab fa-linkedin"></i></a></span>
+					</div>
+				</div>
+				<div class="side_copy_text">
+					<p>© 2023 <a href="#">INVOSOFT (PVT) LTD</a> All Rights Reserved</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Start Breadcrumb Area -->
+	<section class="breadcrumb-area section-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="breadcrumb-content">
+						<h2 style="color: white;">Contact Us</h2>
+						<ul>
+							<li><a href="index.html" style="color: rgb(168, 168, 168);">Home</a></li>
+							<li>Contact Us</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Breadcrumb Area -->
+	<!-- Start Contact Us -->
+	<section class="section-padding">
+		<div class="container">
+			<div class="row mb-30">
+				<!-- Single -->
+				<div class="col-lg-4 col-sm-6 mb-30">
+					<div class="c_info_item">
+						<div class="icon">
+							<i class="fas fa-map-marked-alt"></i>
+						</div>
+						<div class="content">
+							<h4 style="color: #121363;"> Address </h4>
+							<p>Kurfürstendamm 80 Mecklenburg Vorpommern Berlin </p>
+						</div>
+					</div>
+				</div>
+				<!-- Single -->
+				<div class="col-lg-4 col-sm-6 mb-30">
+					<div class="c_info_item">
+						<div class="icon">
+							<i class="fas fa-headset"></i>
+						</div>
+						<div class="content">
+							<h4 style="color: #121363;"> Our Phone </h4>
+							<p> Telephone : (+78) 9102320</p>
+							<p>Mobile : 000 2324 39493 </p>
+						</div>
+					</div>
+				</div>
+				<!-- Single -->
+				<div class="col-lg-4 col-sm-6 mb-30">
+					<div class="c_info_item">
+						<div class="icon">
+							<i class="fas fa-envelope-open"></i>
+						</div>
+						<div class="content">
+							<h4 style="color: #121363;">Email</h4>
+							<p> Office Email : admin@website.com</p>
+							<p>Inquiries : info@domain.com </p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 mb-50 text-center">
+					<div class="section-headding">
+						<h2 style="color: #121363;">Get In Touch</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet maximus est.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2">
+					<div class="contact-form">
+						<form id="contact-form" action="https://preetheme.com/html/cwt/cwt/mail.php" method="POST">
+							<div class="single-input">
+								<label for="name">Your Name</label>
+								<input required="required" type="text" name="name" id="name">
+							</div>
+							<div class="single-input">
+								<label for="email">Your Email</label>
+								<input required="required" type="email" name="email" id="email">
+							</div>
+							<div class="single-input">
+								<label for="subject">Your Subject</label>
+								<input required="required" type="text" name="subject" id="subject">
+							</div>
+							<div class="single-input">
+								<label for="message">Your Message</label>
+								<textarea name="message" id="message" required="required" ></textarea>
+							</div>
+							<div class="single-input">
+								<button class="button-1">Send Message <i class="fas fa-long-arrow-alt-right"></i> <span></span></button>
+							</div>
+						</form>
+						<p class="ajax-response"></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Contact Us -->
+	<!-- Start Footer Area -->
+	<footer class="footer">
+		<!-- Footer Top -->
+		<div class="footer-top pt-70 pb-20">
+			<div class="container">
+				<div class="row">
+					<!-- Single -->
+					<div class="col-lg-3 col-md-6 mb-30">
+						<div class="footer_w_item">
+							<div class="f_logo mb-20">
+								<a href="index.html">
+									<img src="./assets/img/logo-white.png" alt="logo" class="img-fluid">
+								</a>
+							</div>
+							<p>Transform your Ideas into Software Innovations</p>
+							<div class="footer_social">
+								<span><a href="#"><i class="fab fa-facebook-f"></i></a></span>
+								<span><a href="#"><i class="fab fa-twitter"></i></a></span>
+								<span><a href="#"><i class="fab fa-instagram"></i></a></span>
+							</div>
+						</div>
+					</div>
+					<!-- Single -->
+					<div class="col-lg-3 col-md-6 mb-30">
+						<div class="footer_w_item">
+							<h3>Get In Touch</h3>
+							<div class="footer-get-in">
+								<div class="single">
+									<div class="icon">
+										<i class="fas fa-map-marker-alt"></i>
+									</div>
+									<div class="con">
+										<h4>Locations</h4>
+										<p> Lahore, Pakistan</p>
+									</div>
+								</div>
+								<div class="single">
+									<div class="icon">
+										<i class="far fa-envelope"></i>
+									</div>
+									<div class="con">
+										<h4>Email</h4>
+										<p> ahsan@invosoft.com</p>
+									</div>
+								</div>
+								<div class="single">
+									<div class="icon">
+										<i class="fas fa-mobile-alt"></i>
+									</div>
+									<div class="con">
+										<h4>Phone</h4>
+										<p> +92-3494-1084-97</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Single -->
+					<div class="col-lg-3 col-md-6 mb-30">
+						<div class="footer_w_item">
+							<h3>Information</h3>
+							<ul>
+								<li><a href="#">Terms & Condition</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Contact Me</a></li>
+								<li><a href="#">FAQs</a></li>
+								<li><a href="#">Report an issue</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- Single -->
+					<div class="col-lg-3 col-md-6 mb-30">
+						<div class="footer_w_item">
+							<h3>Our Services</h3>
+							<ul>
+								<li><a href="#">Web Design</a></li>
+								<li><a href="#">Web Development</a></li>
+								<li><a href="#">UI/UX Design</a></li>
+								<li><a href="#">App Design & Develop</a></li>
+								<li><a href="#">SEO Marketing</a></li>
+								<li><a href="#">Graphic Design</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Footer Bottom -->
+		<div class="footer-bottom pt-30 pb-30">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="f_copy_text">
+							<p>&copy; 2023	 <strong>INVOSOFT (PVT) LTD</strong>,  All Rights Reserved.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer Area -->
+	<div class="scroll-area">
+       <i class="fa fa-angle-up"></i>
+    </div>
+
+
+    <!-- Js File -->
+    <script src="assets/js/modernizr.min.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/lightcase.js"></script>
+    <script src="assets/js/mixitup.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/typed.js"></script>
+    <script src="assets/js/particles.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/mobile-menu.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+</body>
+
+<!-- Mirrored from preetheme.com/html/cwt/cwt/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 05 Jul 2023 07:00:38 GMT -->
+</html>
